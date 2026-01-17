33267eb9d78c23846728e203dbcb791c3d01cf65
Guido Michieletto <guido@michieletto.it>
2026-01-17 17:49:39 +0100

    fix: updated broken lib showing 3d objects

--- assets/js/appIndex.js
+++ assets/js/appIndex.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.130.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.130.0/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from 'https://cdn.skypack.dev/gsap';
 
 
